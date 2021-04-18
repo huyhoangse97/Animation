eb7ab22e5edc3f83ce2549098f5ceda60abac4e9
huyhoangse97 <80676594+huyhoangse97@users.noreply.github.com>
2021-04-18 20:20:45 +0700

    add clickListener to Animation

--- app/src/main/java/com/example/animation/MainActivity.java
+++ app/src/main/java/com/example/animation/MainActivity.java
@@ -2,18 +2,22 @@ package com.example.animation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 //    AnimationDrawable front_anim, back_anim;
     AnimatorSet front_anim, back_anim, front_anim_re, back_anim_re;
-    ImageButton front_card, back_card;
+    AnimatorSet front_anim_all, back_anim_all;
+    ImageView front_card, back_card;
+    Button btn_flip, btn_spin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
 
         front_card = findViewById(R.id.front_card);
         back_card = findViewById(R.id.back_card);
+        btn_flip = findViewById(R.id.btn_flip);
+        btn_spin = findViewById(R.id.btn_spin);
 
         front_card.setCameraDistance(8000 * scale);
         back_card.setCameraDistance(8000 * scale);
@@ -34,28 +40,110 @@ public class MainActivity extends AppCompatActivity {
         front_anim_re = (AnimatorSet) AnimatorInflater.loadAnimator(this.getApplicationContext(), R.animator.front_animation_reverse);
         back_anim_re = (AnimatorSet) AnimatorInflater.loadAnimator(this.getApplicationContext(), R.animator.back_animation_reverse);
 
+        front_anim_all = (AnimatorSet) AnimatorInflater.loadAnimator(this.getApplicationContext(), R.animator.front_animation_allround);
+        back_anim_all = (AnimatorSet) AnimatorInflater.loadAnimator(this.getApplicationContext(), R.animator.back_animation_allround);
+
         Boolean isMatching = true;
         final Boolean[] isFront = {true};
-        View.OnClickListener cardClickListener = new View.OnClickListener() {
+        View.OnClickListener flipClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Button btn = (Button) v;
                 if (isFront[0] == true){
                     front_anim.setTarget(front_card);
                     back_anim.setTarget(back_card);
+                    Animator.AnimatorListener flipClickListener = new Animator.AnimatorListener() {
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+                            btn.setText("Reserve");
+                            btn_spin.setEnabled(false);
+                            isFront[0] = false;
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationCancel(Animator animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animator animation) {
+
+                        }
+                    };
+                    front_anim.addListener(flipClickListener);
                     front_anim.start();
                     back_anim.start();
-                    isFront[0] = false;
                 }
                 else {
                     back_anim_re.setTarget(back_card);
                     front_anim_re.setTarget(front_card);
+                    Animator.AnimatorListener reserveClickListener = new Animator.AnimatorListener() {
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            isFront[0] = true;
+                            btn.setText("Flip");
+                            btn_spin.setEnabled(true);
+                        }
+
+                        @Override
+                        public void onAnimationCancel(Animator animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animator animation) {
+
+                        }
+                    };
+                    front_anim_re.addListener(reserveClickListener);
                     back_anim_re.start();
                     front_anim_re.start();
-                    isFront[0] = true;
                 }
             }
         };
-        front_card.setOnClickListener(cardClickListener);
-        back_card.setOnClickListener(cardClickListener);
+
+        View.OnClickListener spinClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                back_anim_all.setTarget(back_card);
+                front_anim_all.setTarget(front_card);
+                Animator.AnimatorListener spinListener = new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        btn_flip.setEnabled(false);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        btn_flip.setEnabled(true);
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                };
+                front_anim_all.addListener(spinListener);
+                back_anim_all.start();
+                front_anim_all.start();
+            }
+        };
+
+        btn_flip.setOnClickListener(flipClickListener);
+        btn_spin.setOnClickListener(spinClickListener);
     }
 }
